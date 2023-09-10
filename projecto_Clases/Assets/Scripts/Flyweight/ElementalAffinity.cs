8d0238b99ab0a1102d42c1be0e2873f045964b84
AaronHChoi <108954995+AaronHChoi@users.noreply.github.com>
2023-09-10 17:42:34 -0300

    cambio de projecto devuelta

--- projecto_Clases/Assets/Scripts/Flyweight/ElementalAffinity.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(menuName ="Elements/ElementalAffinity")]
-public class ElementalAffinity : ScriptableObject
-{
-    
-}
